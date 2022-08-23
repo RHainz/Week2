28656a8957df42b9618d51bf588827bbc5270170
RHainz <rogerhainz@gmail.com>
2022-08-23 20:05:35 -0300

    Second

--- tartarugas/assets/index.js
+++ tartarugas/assets/index.js
@@ -6,10 +6,30 @@ canvas.height = 576;
 let pathSize=640;
 let stop = false;
 let round=0;
+let start=0;
+let sono=0;
+let totalApostadoNow=[0,0,0,0,0];
+let thePlayer={
+    nomeJogador: "The Player",
+    goldPlayer: 1000,
+    tartarugaAtual: "Sem Tartaruga"    
+};
 let positionLines={
     x:193,
     y:100
 }
+let positionBackground={
+    x:0,
+    y:0
+}
+
+const backGround= new Sprite({
+    position: {
+        x:0,
+        y:0
+    }, 
+    imgSource: './images/Designsemnome.png'
+});
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -18,31 +38,120 @@ turtle1.draw();
 
 turtle2.draw();
 
+function setZeroGold () {
+    let buttonZero=document.querySelectorAll("#inputsGold input");
+    for (let i=0;i<5;i++){
+        buttonZero[i].valueAsNumber=0;
+        buttonZero[i].value="0";
+    }
+}
+
+function updatePlayer() {
+    let goldPlace=document.querySelector('#gold');
+    let namePlace=document.querySelector('#nome');
+    let tartaruguinha=document.querySelector('#tartaruguinha');
+    namePlace.innerText=`${thePlayer.nomeJogador}`;
+    goldPlace.innerText=`Gold ${thePlayer.goldPlayer}`;
+    tartaruguinha.innerText=`${thePlayer.tartarugaAtual}`;
+}
+
+function updateNameTurtle() {
+    let name1=document.querySelector('#turtle01');
+    let name2=document.querySelector('#turtle02');
+    let name3=document.querySelector('#turtle03');
+    let name4=document.querySelector('#turtle04');
+    let name5=document.querySelector('#turtle05');
+    name1.innerText=`${turtle1.nameTurtle.nome}`;
+    name2.innerText=`${turtle2.nameTurtle.nome}`;
+    //name3.innerText=`${turtle3.nameTurtle.nome}`;
+    //name4.innerText=`${turtle4.nameTurtle.nome}`;        Ligar ao inserir o resto das Tartarugas
+    //name5.innerText=`${turtle5.nameTurtle.nome}`;
+}
+
+function somarGold (){
+    let totalApostado=0;
+    let buttonZero=document.querySelectorAll("#inputsGold input");
+    for (let i=0; i<5; i++){
+        //console.log(buttonZero[i].valueAsNumber);
+        //console.log(totalApostadoNow[i]);
+        if(buttonZero[i].valueAsNumber!==totalApostadoNow[i]) {
+            totalApostado+=(buttonZero[i].valueAsNumber-totalApostadoNow[i]);
+            console.log(`papanapapapapa ${totalApostado}`)
+            thePlayer.goldPlayer-=totalApostado;
+            totalApostadoNow[i]=buttonZero[i].valueAsNumber;
+        }
+    }
+}
+    
+function totalDasApostas() {
+    let totalApostado=0;
+    let buttonZero=document.querySelectorAll("#inputsGold input")
+    for (let i=0; i<5; i++){
+        totalApostado+=buttonZero[i].valueAsNumber;
+}
+    return totalApostado;
+}
+
 function animarTartarugas() {
     window.requestAnimationFrame(animarTartarugas);
     c.fillStyle="black";
     if (stop) { return }
     //c.fillRect(0,0,canvas.width,canvas.height);
+    
     drawingArena();
+    updatePlayer();
+    somarGold();
+    console.log(totalDasApostas());
+//    backGround.update();
     turtle1.update();
     turtle2.update();
+    //turtle3.update();
 }
 
-let i=0; 
-for (let i=0; i<=4; i++) {
-    const ajuste=i*48;
-    console.log (ajuste*10000);
-    c.fillStyle="yellow";
-    c.fillRect(positionLines.x, positionLines.y+ajuste, pathSize, 48);
-    c.fillStyle="black";
-    c.fillRect(positionLines.x+1, positionLines.y+ajuste+1, pathSize-2, 46);
-}
+starteando.addEventListener("click",()=>turtle1.reset());
 
-c.fillStyle="yellow"
-let nomeJogador=c.fillRect(100,400,50,60);
-c.fillText("Nome", 100,400,50,50);
-let posXturtle1=turtle1.positionX()
-c.fillText(`${(posXturtle1)}`,160,400,50,50);
+localizando=document.querySelectorAll("#inputsGold input")
+localizando[0].value="0";
 
+setZeroGold();
+updateNameTurtle();
 console.log(turtle1.positionX());
 animarTartarugas();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i=0; 
+// for (let i=0; i<=4; i++) {
+//     const ajuste=i*48;
+//     console.log (ajuste*10000);
+//     c.fillStyle="yellow";
+//     c.fillRect(positionLines.x, positionLines.y+ajuste, pathSize, 48);
+//     c.fillStyle="black";
+//     c.fillRect(positionLines.x+1, positionLines.y+ajuste+1, pathSize-2, 46);
+// }
+
+// c.fillStyle="yellow"
+// let nomeJogador=c.fillRect(100,400,50,60);
+// c.fillText("Nome", 100,400,50,50);
+// let posXturtle1=turtle1.positionX()
+// c.fillText(`${(posXturtle1)}`,160,400,50,50);
--- tartarugas/assets/script.js
+++ tartarugas/assets/script.js
@@ -1,9 +1,12 @@
 class Turtle {
-    constructor ({position, velocidade, rndNumber}) {
+    constructor ({position, velocidade, rndNumber, nameTurtle, resistencia, conditions}) {
         this.position = position;
         this.velocidade = velocidade;
         this.rndNumber = rndNumber;
         this.stop=false;
+        this.nameTurtle=nameTurtle;
+        this.resistencia=resistencia;
+        this.conditions=conditions;
     }
 
     draw () {
@@ -13,26 +16,84 @@ class Turtle {
         c.fillText(`${this.position.x}`,this.position.x,this.position.y);
     }
 
+    resistenciaTeste () {
+        this.rndNumber=randomizer("res");
+        //console.log(`${this.rndNumber} mamamamamamaqiieuwiueiwueiuw`);
+        if (this.rndNumber>this.resistencia.stamina && !this.conditions.bestificada ) {
+            return this.conditions.tired="true";            
+        }
+    }
+
+
     update () {
-        this.draw()
-        this.rndNumber=randomizer();
+        this.draw();
+        this.resistenciaTeste();
+        this.rndNumber=randomizer("");
         //console.log(this.rndNumber);
         round=round+1;
-        console.log(round);
+        
+        console.log(start);
+        //console.log(this.conditions.bestificada);
+        if(this.conditions.tired){
+            this.velocidade.x=0;
+            start=start+1;
+            if (start>100){
+                sono ++;
+                start=0;
+                this.conditions.tired=false;
+            }
+        }else{ 
         this.position.x +=this.velocidade.x+this.rndNumber;
         if (this.position.x >=(pathSize+192)) {
             console.log("vencedora");
+            console.log(sono);
             this.velocidade.x=0;
             this.position.x-=this.rndNumber;
             return stop=true;
         }
     }
-    
+    }
+
+    reset () {
+        this.position.x=194;
+    }
+
     positionX() {
         return this.position.x;
     }
+}
 
-    }
+
+
+
+
+
+
+
+    class Sprite {
+        contructor({position, imgSource}) {
+            this.position = position;
+            this.width= 50;
+            this.heigth= 100;
+            this.image= new Image();
+            this.image.src=imgSource;
+        }
+
+        draw(){
+            c.drawImage(this.image, this.position.x, this.position.y);
+        }
+
+        update(){
+            this.draw();
+        }
+
+}
+
+
+
+
+
+    
 
 // class DisplayJogador{
 //     constructor ({nome}){
@@ -42,20 +103,29 @@ class Turtle {
 //     ()
 // }
 
-
-
 const turtle1= new Turtle ({
     position: {
         x:194,
         y:108
     },
     velocidade: {
-        x:+0.0001,
+        x:+0.003,
         y:0
     },
     rndNumber: {
         x:0,
         y:0
+    },
+    nameTurtle: {
+        nome: "Rogeruga"
+    },
+    conditions: {
+        tired:false,
+        bestificada: false
+    },
+    resistencia: {
+        stamina: 99950,
+        foco: 4999
     }
 });
 
@@ -67,18 +137,34 @@ const turtle2= new Turtle ({
     y:156
     },
     velocidade: {
-        x:+0.0009,
+        x:+0.009,
         y:0
     },
     rndNumber: {
         x:0,
         y:0
+    },
+    nameTurtle: {
+        nome: "Marcoruga"
+    },
+    conditions: {
+        tired:false,
+        bestificada: false
+    },
+    resistencia: {
+        stamina: 99960,
+        foco: 99960
     }
 });
 
-function randomizer() {
-    return (Math.floor(Math.random()*100+1)/200);
+function randomizer(tipo) {
+    if (tipo=="res") {return ((Math.random()*100000+1))}
+    else {
+        return (Math.floor(Math.random()*100+1)/200)
+    };
 }
+
+
 
 function drawingArena () {
     for (let i=0; i<=4; i++) {
